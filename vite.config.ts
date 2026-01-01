7a6bfd31c67f90499fcc3f17d6e16a809c656a38
Mubashirshah04 <mobiletesting736@gmail.com>
2026-01-01 21:51:28 +0500

    Add files via upload
    Inshalllah

--- vite.config.ts
+++ vite.config.ts
@@ -5,29 +5,19 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "./",
   server: {
     host: "::",
     port: 8080,
-    headers: {
-      'Content-Type': 'application/javascript; charset=utf-8',
-    },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
-      "@": path.resolve("./src"),
+      "@": "/src",
     },
   },
   build: {
-    assetsInlineLimit: 0,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-        entryFileNames: 'assets/[name]-[hash].js',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash].[ext]',
-      },
-    },
+    outDir: "dist",
+    assetsDir: "assets",
+    emptyOutDir: true,
   },
 }));
